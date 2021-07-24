e89723189a5833632ac2b650ceae90cca6ac5e5b
Sergei <serheifil@gmail.com>
2021-07-24 15:25:28 +0300

    Updates

---
+++ precache-manifest.717a444b0489093a8ce8484b193cffa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bfc33cdaf4ff9d56acf037d1fbb874",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d2829b0a988d6d2b0f9",
+    "url": "./static/css/main.d51da3b2.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2829b0a988d6d2b0f9",
+    "url": "./static/js/main.dedae4f2.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
